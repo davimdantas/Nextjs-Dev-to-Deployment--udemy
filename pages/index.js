2ced523424b00f91451a8d90f98c416fbcd93347
davimdantas <davimdantas@gmail.com>
2021-06-02 12:57:19 -0300

    15 - getserverSideProps & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+export default function HomePage({ events }) {
+	console.log('events:', events)
+	return (
+		<Layout>
+			<h1>Upcoming Events</h1>
+		</Layout>
+	)
+}
 
-export default function HomePage() {
-  return (
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
-  )
+// export async function getServerSideProps() {
+// 	const res = await fetch(`${API_URL}/api/events`)
+// 	const events = await res.json()
+
+// 	return {
+// 		props: { events },
+// 	}
+// }
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`)
+	const events = await res.json()
+
+	return {
+		props: { events },
+		revalidate: 1,
+	}
 }
