c5a2340803a845818692ff7a2fc3be81e6bffd76
davimdantas <davimdantas@gmail.com>
2021-06-02 21:21:53 -0300

    18 - Single Event Page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,50 @@
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 
 export default function EventPage({ evt }) {
+	const deleteEvent = (e) => {}
+
 	return (
 		<Layout>
-			<h1> {evt.name} </h1>
+			<div className={styles.event}>
+				<div className={styles.controls}>
+					<Link href={`/events/edit/${evt.id}`}>
+						<a>
+							<FaPencilAlt /> Edit Event
+						</a>
+					</Link>
+					<a href='#' className={styles.delete} onClick={deleteEvent}>
+						<FaTimes /> Delete Events
+					</a>
+				</div>
+
+				<span>
+					{evt.date} at {evt.time}
+				</span>
+				<h1>{evt.name}</h1>
+				{evt.image && (
+					<div className={styles.image}>
+						<Image src={evt.image} width={960} height={600} />
+					</div>
+				)}
+
+				<h3>Performers:</h3>
+				<p>{evt.performers}</p>
+
+				<h3>Description:</h3>
+				<p>{evt.description}</p>
+
+				<h3>Venue: {evt.venue}</h3>
+				<p>{evt.address}</p>
+
+				<Link href='/events'>
+					<a className={styles.back}>{'<'} Go Back</a>
+				</Link>
+			</div>
 		</Layout>
 	)
 }
@@ -16,7 +56,7 @@ export async function getStaticPaths() {
 	const paths = events.map((evt) => {
 		return { params: { slug: evt.slug } }
 	})
-	console.log('paths:', paths)
+
 	return {
 		paths, //indicates that no page needs be created at build time
 		fallback: true, //indicates the type of fallback
@@ -27,7 +67,6 @@ export async function getStaticProps({ params }) {
 	const { slug } = params
 	const res = await fetch(`${API_URL}/api/events/${slug}`)
 	const evt = await res.json()
-	console.log('evt:', evt)
 
 	return {
 		props: { evt },
